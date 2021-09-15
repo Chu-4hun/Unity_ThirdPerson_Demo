eef059bb23ce3e9f98c01ce69870e4bc46f0f316
Chu-4hun <70685495+Chu-4hun@users.noreply.github.com>
2021-09-15 15:08:28 +0300

    Complete BoxPlayerMovement.cs

--- Assets/Scripts/BoxPlayer/BoxPlayerMovement.cs
+++ Assets/Scripts/BoxPlayer/BoxPlayerMovement.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 // ReSharper disable once CheckNamespace
 public class BoxPlayerMovement : MonoBehaviour
 {
+    public float jumpForce = 1000f;
     public Rigidbody rb;
+    public float speed = 1000f;
+    public int jumpAmount = 2;
+    private bool is_ground = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +22,29 @@ public class BoxPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0,0,200 * Time.deltaTime);
+        
+        
     }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        rb.AddForce(direction * (speed * Time.deltaTime));
+        
+        if (Input.GetButtonDown("Jump") && is_ground)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+        
+    }
+
+    void OnTriggerStay(Collider col){
+        if (col.CompareTag("floor")) is_ground = true;      
+    }
+    void OnTriggerExit(Collider col){
+        if (col.CompareTag("floor")) is_ground = false;    
+    }
+    
 }
